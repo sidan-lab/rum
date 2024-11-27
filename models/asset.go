512053031a3be733ce4dfb32c46b833403ed4e30
SIDANWhatever <wongkahinhinson@gmail.com>
2024-11-28 02:24:59 +0800

    feat: add value

--- models/asset.go
+++ models/asset.go
@@ -1,5 +1,7 @@
 package models
 
+import "strconv"
+
 type Asset struct {
 	Unit     string `json:"unit" binding:"required"`
 	Quantity string `json:"quantity" binding:"required"`
@@ -22,4 +24,29 @@ func (a *Assets) PopAssetByUnit(unit string) Asset {
 	}
 	a = &filteredAssets
 	return filteredAsset
+}
+
+func (a *Assets) MergeAssets(assets []Asset) []Asset {
+	mergedAssets := make(map[string]Asset)
+	for _, asset := range assets {
+		if existingAsset, ok := mergedAssets[asset.Unit]; ok {
+			existingAsset.Quantity = AddQuantities(existingAsset.Quantity, asset.Quantity)
+			mergedAssets[asset.Unit] = existingAsset
+		} else {
+			mergedAssets[asset.Unit] = asset
+		}
+	}
+	result := make([]Asset, 0, len(mergedAssets))
+	for _, asset := range mergedAssets {
+		result = append(result, asset)
+	}
+	return result
+}
+
+func AddQuantities(quantity1 string, quantity2 string) string {
+	intQuantity1, _ := strconv.Atoi(quantity1)
+	intQuantity2, _ := strconv.Atoi(quantity2)
+	sum := intQuantity1 + intQuantity2
+	stringSum := strconv.Itoa(sum)
+	return stringSum
 }
---
+++ models/value.go
@@ -0,0 +1,96 @@
+package models
+
+import (
+	"strconv"
+)
+
+type Value struct {
+	Value map[string]int64
+}
+
+func NewValue() *Value {
+	return &Value{
+		Value: make(map[string]int64),
+	}
+}
+
+func (mv *Value) AddAsset(asset Asset) {
+	quantity, _ := strconv.ParseInt(asset.Quantity, 10, 64)
+	if existingQuantity, exists := mv.Value[asset.Unit]; exists {
+		mv.Value[asset.Unit] = existingQuantity + quantity
+	} else {
+		mv.Value[asset.Unit] = quantity
+	}
+}
+
+func (mv *Value) AddAssets(assets []Asset) {
+	for _, asset := range assets {
+		mv.AddAsset(asset)
+	}
+}
+
+func (mv *Value) NegateAsset(asset Asset) {
+	quantity, _ := strconv.ParseInt(asset.Quantity, 10, 64)
+	if existingQuantity, exists := mv.Value[asset.Unit]; exists {
+		newQuantity := existingQuantity - quantity
+		if newQuantity == 0 {
+			delete(mv.Value, asset.Unit)
+		} else {
+			mv.Value[asset.Unit] = newQuantity
+		}
+	}
+}
+
+func (mv *Value) NegateAssets(assets []Asset) {
+	for _, asset := range assets {
+		mv.NegateAsset(asset)
+	}
+}
+
+func (mv *Value) Get(unit string) int64 {
+	if quantity, exists := mv.Value[unit]; exists {
+		return quantity
+	}
+	return 0
+}
+
+func (mv *Value) Units() []string {
+	units := make([]string, 0, len(mv.Value))
+	for unit := range mv.Value {
+		units = append(units, unit)
+	}
+	return units
+}
+
+func (mv *Value) IsEmpty() bool {
+	return len(mv.Value) == 0
+}
+
+func (mv *Value) Merge(values ...*Value) {
+	for _, other := range values {
+		for unit, quantity := range other.Value {
+			if existingQuantity, exists := mv.Value[unit]; exists {
+				mv.Value[unit] = existingQuantity + quantity
+			} else {
+				mv.Value[unit] = quantity
+			}
+		}
+	}
+}
+
+func (mv *Value) ToAssets() []Asset {
+	assets := make([]Asset, 0, len(mv.Value))
+	for unit, quantity := range mv.Value {
+		assets = append(assets, Asset{
+			Unit:     unit,
+			Quantity: strconv.FormatInt(quantity, 10),
+		})
+	}
+	return assets
+}
+
+func FromAssets(assets []Asset) *Value {
+	mv := NewValue()
+	mv.AddAssets(assets)
+	return mv
+}
