0c220ed9ddfe9ae739de310863332df79f2ea71a
Newt <newt@Newts-MacBook-Air.local>
2025-06-20 16:46:54 +0800

    feat: complete certificate api
    fix: unused import

--- builder/certificate.go
+++ builder/certificate.go
@@ -198,6 +198,171 @@ func (builder *TxBuilder) DRepUpdate(
 	return builder
 }
 
-// pub fn certificate_script
-// pub fn certificate_tx_in_reference
-// pub fn certificate_redeemer_value
+func (builder *TxBuilder) CertificateScript(scriptCbor string, version *types.LanguageVersion) *TxBuilder {
+	// Pop
+	lastCert := builder.TxBuilderBody.Certificates[len(builder.TxBuilderBody.Certificates)-1]
+	builder.TxBuilderBody.Certificates = builder.TxBuilderBody.Certificates[:len(builder.TxBuilderBody.Certificates)-1]
+	if lastCert != nil {
+		panic("Undefined certificate")
+	}
+	switch cert := lastCert.(type) {
+	case types.BasicCertificate:
+		if version != nil {
+			builder.TxBuilderBody.Certificates = append(
+				builder.TxBuilderBody.Certificates,
+				types.ScriptCertificate{
+					Cert:     cert.Inner,
+					Redeemer: nil,
+					ScriptSource: types.ProvidedScriptSource{
+						ScriptCbor:      scriptCbor,
+						LanguageVersion: *version,
+					},
+				},
+			)
+		} else {
+			builder.TxBuilderBody.Certificates = append(
+				builder.TxBuilderBody.Certificates,
+				types.SimpleScriptCertificate{
+					Cert: cert.Inner,
+					SimpleScriptSource: types.ProvidedSimpleScriptSource{
+						ScriptCbor: scriptCbor,
+					},
+				})
+		}
+	case types.ScriptCertificate:
+		if version == nil {
+			panic("Language version has to be defined for plutus certificates")
+		}
+		builder.TxBuilderBody.Certificates = append(
+			builder.TxBuilderBody.Certificates,
+			types.ScriptCertificate{
+				Cert:     cert.Cert,
+				Redeemer: cert.Redeemer,
+				ScriptSource: types.ProvidedScriptSource{
+					ScriptCbor:      scriptCbor,
+					LanguageVersion: *version,
+				},
+			})
+	case types.SimpleScriptCertificate:
+		panic("Native script cert had its script defined twice")
+	}
+	return builder
+}
+
+func (builder *TxBuilder) CertificateTxInReference(
+	txHash string,
+	txIndex uint32,
+	scriptHash string,
+	version *types.LanguageVersion,
+	scriptSize uint,
+) *TxBuilder {
+	// Pop
+	lastCert := builder.TxBuilderBody.Certificates[len(builder.TxBuilderBody.Certificates)-1]
+	builder.TxBuilderBody.Certificates = builder.TxBuilderBody.Certificates[:len(builder.TxBuilderBody.Certificates)-1]
+	if lastCert != nil {
+		panic("Undefined certificate")
+	}
+	switch cert := lastCert.(type) {
+	case types.BasicCertificate:
+		if version != nil {
+			builder.TxBuilderBody.Certificates = append(
+				builder.TxBuilderBody.Certificates,
+				types.ScriptCertificate{
+					Cert:     cert.Inner,
+					Redeemer: nil,
+					ScriptSource: types.InlineScriptSource{
+						RefTxIn: types.RefTxIn{
+							TxHash:  txHash,
+							TxIndex: txIndex,
+							// Script size is already accounted for in script source
+							ScriptSize: nil,
+						},
+						ScriptHash:      scriptHash,
+						LanguageVersion: *version,
+						ScriptSize:      scriptSize,
+					},
+				},
+			)
+		} else {
+			builder.TxBuilderBody.Certificates = append(
+				builder.TxBuilderBody.Certificates,
+				types.SimpleScriptCertificate{
+					Cert: cert.Inner,
+					SimpleScriptSource: types.InlineSimpleScriptSource{
+						RefTxIn: types.RefTxIn{
+							TxHash:  txHash,
+							TxIndex: txIndex,
+							// Script size is already accounted for in script source
+							ScriptSize: nil,
+						},
+						SimpleScriptHash: scriptHash,
+						ScriptSize:       scriptSize,
+					},
+				})
+		}
+	case types.ScriptCertificate:
+		if version == nil {
+			panic("Language version has to be defined for plutus certificates")
+		}
+		builder.TxBuilderBody.Certificates = append(
+			builder.TxBuilderBody.Certificates,
+			types.ScriptCertificate{
+				Cert:     cert.Cert,
+				Redeemer: cert.Redeemer,
+				ScriptSource: types.InlineScriptSource{
+					RefTxIn: types.RefTxIn{
+						TxHash:  txHash,
+						TxIndex: txIndex,
+						// Script size is already accounted for in script source
+						ScriptSize: nil,
+					},
+					ScriptHash:      scriptHash,
+					LanguageVersion: *version,
+					ScriptSize:      scriptSize,
+				},
+			})
+	case types.SimpleScriptCertificate:
+		panic("Native script cert had its script defined twice")
+	}
+	return builder
+}
+
+func (builder *TxBuilder) CertificateRedeemerValue(redeemer WRedeemer) *TxBuilder {
+	// Pop
+	lastCert := builder.TxBuilderBody.Certificates[len(builder.TxBuilderBody.Certificates)-1]
+	builder.TxBuilderBody.Certificates = builder.TxBuilderBody.Certificates[:len(builder.TxBuilderBody.Certificates)-1]
+	if lastCert != nil {
+		panic("Undefined certificate")
+	}
+	rawRedeemer, err := redeemer.Data.ToCbor()
+	if err != nil {
+		panic("Error converting certificate redeemer to CBOR")
+	}
+	currentRedeemer := types.Redeemer{
+		Data:    rawRedeemer,
+		ExUnits: redeemer.ExUnits,
+	}
+	switch cert := lastCert.(type) {
+	case types.BasicCertificate:
+		builder.TxBuilderBody.Certificates = append(
+			builder.TxBuilderBody.Certificates,
+			types.ScriptCertificate{
+				Cert:         cert.Inner,
+				Redeemer:     &currentRedeemer,
+				ScriptSource: nil,
+			},
+		)
+	case types.ScriptCertificate:
+		builder.TxBuilderBody.Certificates = append(
+			builder.TxBuilderBody.Certificates,
+			types.ScriptCertificate{
+				Cert:         cert.Cert,
+				Redeemer:     &currentRedeemer,
+				ScriptSource: cert.ScriptSource,
+			},
+		)
+	case types.SimpleScriptCertificate:
+		panic("Native script cert cannot use redeemers")
+	}
+	return builder
+}
