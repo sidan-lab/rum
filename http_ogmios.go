c9c0ea3f1be4ad370bc4816f3871fdbbce722bbe
Ken Lau <kenlauhong2015@gmail.com>
2026-03-22 16:00:35 +0800

    fix: update HttpOgmiosProvider for Ogmios v6 API

--- http_ogmios.go
+++ http_ogmios.go
@@ -1,20 +1,30 @@
 package rum
 
 import (
-	"github.com/ethereum/go-ethereum/rpc"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 type HttpOgmiosProvider struct {
-	*rpc.Client
+	baseUrl    string
+	httpClient *http.Client
 }
 
 func NewHttpOgmiosProvider(baseUrl string) ISubmitter {
-	// assume correct url
-	rpcClient, _ := rpc.Dial(baseUrl)
-
 	return &HttpOgmiosProvider{
-		rpcClient,
+		baseUrl:    baseUrl,
+		httpClient: &http.Client{},
 	}
+}
+
+type ogmiosRequest struct {
+	Jsonrpc string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	ID      int         `json:"id"`
 }
 
 type SubmitTxResult struct {
@@ -23,18 +33,50 @@ type SubmitTxResult struct {
 	} `json:"transaction"`
 }
 
+type ogmiosResponse struct {
+	Result SubmitTxResult `json:"result"`
+	Error  *struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"error"`
+}
+
 func (o *HttpOgmiosProvider) SubmitTx(txCbor string) (string, error) {
-	var result SubmitTxResult
-	params := map[string]interface{}{
-		"transaction": map[string]string{
-			"cbor": txCbor,
+	reqBody := ogmiosRequest{
+		Jsonrpc: "2.0",
+		Method:  "submitTransaction",
+		Params: map[string]interface{}{
+			"transaction": map[string]string{
+				"cbor": txCbor,
+			},
 		},
+		ID: 1,
 	}
 
-	err := o.Client.Call(&result, "submitTransaction", params)
+	jsonData, err := json.Marshal(reqBody)
 	if err != nil {
 		return "", err
 	}
 
-	return result.Transaction.ID, nil
+	resp, err := o.httpClient.Post(o.baseUrl, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var ogmiosResp ogmiosResponse
+	if err := json.Unmarshal(body, &ogmiosResp); err != nil {
+		return "", fmt.Errorf("failed to parse response: %w, body: %s", err, string(body))
+	}
+
+	if ogmiosResp.Error != nil {
+		return "", fmt.Errorf("%d: %s", ogmiosResp.Error.Code, ogmiosResp.Error.Message)
+	}
+
+	return ogmiosResp.Result.Transaction.ID, nil
 }
